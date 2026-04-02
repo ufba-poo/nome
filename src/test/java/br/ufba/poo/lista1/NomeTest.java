fc6cf4f6118d8bd8ce262cbb388f04c7436d21ec
Rodrigo Rocha <rodrigorgs@gmail.com>
2026-04-02 09:43:50 -0300

    Adiciona testes

--- src/test/java/br/ufba/poo/lista1/NomeTest.java
+++ src/test/java/br/ufba/poo/lista1/NomeTest.java
@@ -12,5 +12,37 @@ public class NomeTest {
         nome.setSobrenome("de Tal");
         assertEquals("Fulano de Tal", nome.getNomeCompleto());
     }
+
+    @Test
+    public void testaAlterarNome() {
+        String s1 = "iuw e hqwe";
+        String s2 = "qw ieg as";
+        Nome nome = new Nome();
+        nome.setPrimeiroNome("abc");
+        nome.setSobrenome(s2);
+        nome.setPrimeiroNome(s1);
+        assertEquals(s1 + " " + s2, nome.getNomeCompleto());
+    }
+
+    @Test
+    public void testaGettersESetters() {
+        String s1 = "iscuw e hqwe";
+        String s2 = "qw utyieg as";
+        Nome nome = new Nome();
+        nome.setPrimeiroNome(s1);
+        nome.setSobrenome(s2);
+        assertEquals(s1, nome.getPrimeiroNome());
+        assertEquals(s2, nome.getSobrenome());
+    }
+
+    @Test
+    public void testaOutroExemplo() {
+        String s1 = "iuw e hqwe";
+        String s2 = "qw ieg as";
+        Nome nome = new Nome();
+        nome.setPrimeiroNome(s1);
+        nome.setSobrenome(s2);
+        assertEquals(s1 + " " + s2, nome.getNomeCompleto());
+    }
 }
 
